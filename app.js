e81fbf1fe5625929940f1ad9ea90bba63b6f5c41
Edwin Mrtz <edwin.martinez.tech@gmail.com>
2020-11-14 13:02:05 -0800

    add a when method to ask a question when the answer is yes or no

--- app.js
+++ app.js
@@ -39,7 +39,14 @@ const promptUser = () => {
             type: 'input',
             name: 'about',
             message: 'Provide some information about yourself:',
-            when: ({ confirmAbout }) => confirmAbout
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+
+            }
         }
     ]);
 };
